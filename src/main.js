1769353ae47ca7c691f909828b100c5f6f21784a
Cary Huang <ckhuang@ucsc.edu>
2020-04-22 05:58:24 -0400

    Included points breakdown in comments in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/*
+Cary Huang(ckhuang)
+Points Breakdown:
+    Track a high score that persists across scenes and display it in the UI (10)
+    Add your own (copyright-free) background music to the Play scene (10)
+    implement the speed increase that happens after 30 seconds in the original game (10)
+    Allow the player to control the Rocket after it's fired (10)
+    Create a new title screen (15)
+    Implement a new timing/scoring mechanism that adds time to the clock for successful hits (25)
+    Implement a simultaneous two-player mode (50)
+*/
+
 // game configuration object
 let config = {
     type: Phaser.CANVAS,
