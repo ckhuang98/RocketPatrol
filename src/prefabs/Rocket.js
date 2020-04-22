ab0f46954faf84784702c16461306443d6ebb062
Cary Huang <ckhuang@ucsc.edu>
2020-04-22 01:36:40 -0400

    fixed error

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -9,12 +9,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     update() {
         // left/right movement
-        if (!this.isFiring) {
-            if (keyLEFT.isDown && this.x >= 47) {
+        if (keyLEFT.isDown && this.x >= 47) {
                 this.x -= 2;
-            } else if (keyRIGHT.isDown && this.x <= 598) {
+        } else if (keyRIGHT.isDown && this.x <= 598) {
                 this.x += 2;
-            }
         }
         // fire button
         if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
